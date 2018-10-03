a47473837d0446edd6b2a8aa030ccad72180350e
Luiz Fernando <luizferbr17@hotmail.com>
2018-10-02 22:27:36 -0300

    IRC dos servidore de OpenSpades para o Discord e outras coisas

--- Bot/Program.cs
+++ Bot/Program.cs
@@ -27,8 +27,6 @@ using System.Reflection;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-using System.Net;
-using System.Net.Sockets;
 using Wall_E.Comandos;
 using Wall_E.Utilidades;
 using Wall_E.Bot;
@@ -38,27 +36,6 @@ namespace Wall_E
 {
     public class Wall_E
     {
-        //TOW
-        public static string IP = "irc.quakenet.org";
-        private static int Porta = 6667;
-        private static string Usuario = "USER IRCbot 0 * :IRCbot";
-        private static string Nome = "Wall-E";
-        private static string Canal = "#ubge.servidor";
-
-        //Arena
-        public static string IP2 = "irc.quakenet.org";
-        private static int Porta2 = 6667;
-        private static string Usuario2 = "USER IRCbot 0 * :IRCbot";
-        private static string Nome2 = "Wall-E";
-        private static string Canal2 = "#ubge.servidor2";
-
-        //Semanal
-        public static string IP3 = "irc.quakenet.org";
-        private static int Porta3 = 6667;
-        private static string Usuario3 = "USER IRCbot 0 * :IRCbot";
-        private static string Nome3 = "Wall-E";
-        private static string Canal3 = "#ubge.servidor3";
-        
         public static IContainer Services
         {
             private set;
@@ -155,142 +132,6 @@ namespace Wall_E
             {
                 await Discord.UpdateStatusAsync(new DiscordGame("no Discord da UBGE!"));
                 await Log.SendMessageAsync($"**Wall-E da Ética online!**\nLigado às: `{DateTime.Now}`");
-
-                #region IRC do TOW -> Discord
-                DiscordChannel TOW_Chat = Discord.GetChannelAsync(valores.tow_chat).Result;
-                DiscordChannel Secretaria_OpenSpades = Discord.GetChannelAsync(valores.secretaria_openspades_chat).Result;
-
-                NetworkStream NS;
-                TcpClient IRC;
-                string InputLine;
-                StreamReader Reader;
-                StreamWriter Writer;
-
-                try
-                {
-                    IRC = new TcpClient(IP, Porta);
-                    NS = IRC.GetStream();
-                    Reader = new StreamReader(NS);
-                    Writer = new StreamWriter(NS);
-                    Writer.WriteLine($"NICK {Nome}");
-                    Writer.Flush();
-                    Writer.WriteLine(Usuario);
-                    Writer.Flush();
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    Console.WriteLine($"[IRC] [UBGE-TOW] [Wall-E] [Discord] | A conexão com o servidor: \"TOW\" foi estabelecida com sucesso!");
-                    Console.ResetColor();
-                    await Secretaria_OpenSpades.SendMessageAsync($"**[IRC] [UBGE-TOW] [Wall-E] [Discord]** **|** A conexão com o servidor: **\"TOW\"** foi estabelecida com sucesso! Chat sendo enviado no: <#{valores.tow_chat}>");
-
-                    while (true)
-                    {
-                        while ((InputLine = Reader.ReadLine()) != null)
-                        {
-                            await TOW_Chat.SendMessageAsync($"**[UBGE-TOW]** **|** ``{DateTime.Now}`` >> {InputLine.Replace(":UBGE-ToW!~UBGE-ToW@179.218.243.249 PRIVMSG #ubge.servidor :", "")}");
-                            string[] splitInput = InputLine.Split(new Char[] {
-                            ' '
-                        });
-                            if (splitInput[0] == "PING")
-                            {
-                                string Resposta = splitInput[1];
-                                Writer.WriteLine($"PONG {Resposta}");
-                                Writer.Flush();
-                            }
-
-                            switch (splitInput[1])
-                            {
-                                case "001":
-                                    Writer.WriteLine($"JOIN {Canal}");
-                                    Writer.Flush();
-                                    break;
-                                default:
-                                    break;
-                            }
-                        }
-                        Writer.Close();
-                        Reader.Close();
-                        IRC.Close();
-                    }
-                }
-                catch (Exception ex)
-                {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine($"[IRC] [UBGE-TOW] [Wall-E] [Discord] | A conexão com o servidor: \"TOW\" não foi estabelecida com sucesso.\n.\nErro: {ex.ToString()}");
-                    Console.ResetColor();
-                    await Secretaria_OpenSpades.SendMessageAsync($"**[IRC] [UBGE-TOW] [Wall-E] [Discord]** **|** A conexão com o servidor: **\"TOW\"** não foi estabelecida com sucesso. <@&{valores.OpenSpades}>, procurem o erro e tentem resolver!\n.\n**Erro:** {ex.ToString()}");
-                    await Log.SendMessageAsync($"**[IRC] [UBGE-TOW] [Wall-E] [Discord]** **|** A conexão com o servidor: **\"TOW\"** não foi estabelecida com sucesso.\n.\n**Erro:** {ex.ToString()}");
-                    Thread.Sleep(5000);
-                    string[] argv = { };
-                }
-                #endregion
-
-                #region IRC da Arena -> Discord
-                DiscordChannel Arena_Chat = Discord.GetChannelAsync(valores.arena_chat).Result;
-                DiscordChannel Secretaria_OpenSpades2 = Discord.GetChannelAsync(valores.secretaria_openspades_chat).Result;
-
-                NetworkStream NS2;
-                TcpClient IRC2;
-                string InputLine2;
-                StreamReader Reader2;
-                StreamWriter Writer2;
-
-                try
-                {
-                    IRC2 = new TcpClient(IP2, Porta2);
-                    NS2 = IRC2.GetStream();
-                    Reader2 = new StreamReader(NS2);
-                    Writer2 = new StreamWriter(NS2);
-                    Writer2.WriteLine($"NICK {Nome2}");
-                    Writer2.Flush();
-                    Writer2.WriteLine(Usuario);
-                    Writer2.Flush();
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    Console.WriteLine($"[IRC] [UBGE-Arena] [Wall-E] [Discord] | A conexão com o servidor: \"Arena\" foi estabelecida com sucesso!");
-                    Console.ResetColor();
-                    await Secretaria_OpenSpades.SendMessageAsync($"**[IRC] [UBGE-Arena] [Wall-E] [Discord]** **|** A conexão com o servidor: **\"Arena\"** foi estabelecida com sucesso! Chat sendo enviado no: <#{valores.arena_chat}>");
-
-                    while (true)
-                    {
-                        while ((InputLine2 = Reader2.ReadLine()) != null)
-                        {
-                            await Arena_Chat.SendMessageAsync($"**[UBGE-Arena]** **|** ``{DateTime.Now}`` >> {InputLine2.Replace(":UBGE-Arena!~UBGE-Arena@179.218.243.249 PRIVMSG #ubge.servidor :", "")}");
-                            string[] splitInput2 = InputLine2.Split(new Char[] {
-                            ' '
-                        });
-                            if (splitInput2[0] == "PING")
-                            {
-                                string Resposta2 = splitInput2[1];
-                                Writer2.WriteLine($"PONG {Resposta2}");
-                                Writer2.Flush();
-                            }
-
-                            switch (splitInput2[1])
-                            {
-                                case "001":
-                                    Writer2.WriteLine($"JOIN {Canal2}");
-                                    Writer2.Flush();
-                                    break;
-                                default:
-                                    break;
-                            }
-                        }
-                        Writer2.Close();
-                        Reader2.Close();
-                        IRC2.Close();
-                    }
-                }
-                catch (Exception ex2)
-                {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine($"[IRC] [UBGE-Arena] [Wall-E] [Discord] | A conexão com o servidor: \"Arena\" não foi estabelecida com sucesso.\n.\nErro: {ex2.ToString()}");
-                    Console.ResetColor();
-                    await Secretaria_OpenSpades.SendMessageAsync($"**[IRC] [UBGE-Arena] [Wall-E] [Discord]** **|** A conexão com o servidor: **\"Arena\"** não foi estabelecida com sucesso. <@&{valores.OpenSpades}>, procurem o erro e tentem resolver!\n.\n**Erro:** {ex2.ToString()}");
-                    await Log.SendMessageAsync($"**[IRC] [UBGE-Arena] [Wall-E] [Discord]** **|** A conexão com o servidor: **\"Arena\"** não foi estabelecida com sucesso.\n.\n**Erro:** {ex2.ToString()}");
-                    Thread.Sleep(5000);
-                    string[] argv = { };
-                }
-                #endregion
-
-                //Falta o Semanal
             }
 
             CommandsNext.RegisterCommands(Assembly.GetEntryAssembly());
---
+++ Comandos/Extras das Secretarias/IRC - SecretariaOpenSpades/IRC_TOW.cs
@@ -0,0 +1,96 @@
+﻿using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.CommandsNext;
+using System.Net;
+using System.Net.Sockets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DSharpPlus.Entities;
+using System.IO;
+using System.Threading;
+
+namespace Wall_E.Comandos.Extras_das_Secretarias
+{
+    public class IRC_TOW
+    {
+        public static string IP = "irc.quakenet.org";
+        private static int Porta = 6667;
+        private static string Usuario = "USER IRCbot 0 * :IRCbot";
+        private static string Nome = "Wall-E";
+        private static string Canal = "#ubge.servidor";
+
+        [Command("towirc")]
+        [Aliases("TOWIRC", "TowIRC")]
+
+        public async Task IRC_do_TOW(CommandContext ctx)
+        {
+            DiscordChannel TOW_Chat = ctx.Guild.GetChannel(valores.tow_chat);
+            DiscordChannel Secretaria_OpenSpades = ctx.Guild.GetChannel(valores.secretaria_openspades_chat);
+            DiscordChannel Log = ctx.Guild.GetChannel(460875622323978240);
+
+            NetworkStream NS;
+            TcpClient IRC;
+            string InputLine;
+            StreamReader Reader;
+            StreamWriter Writer;
+
+            try
+            {
+                IRC = new TcpClient(IP, Porta);
+                NS = IRC.GetStream();
+                Reader = new StreamReader(NS);
+                Writer = new StreamWriter(NS);
+                Writer.WriteLine($"NICK {Nome}");
+                Writer.Flush();
+                Writer.WriteLine(Usuario);
+                Writer.Flush();
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"[IRC] [UBGE-TOW] [Wall-E] [Discord] | A conexão com o servidor: \"TOW\" foi estabelecida com sucesso!");
+                Console.ResetColor();
+                await Secretaria_OpenSpades.SendMessageAsync($"**[IRC] [UBGE-TOW] [Wall-E] [Discord]** **|** A conexão com o servidor: **\"TOW\"** foi estabelecida com sucesso! Chat sendo enviado no: <#{valores.tow_chat}>");
+
+                while (true)
+                {
+                    while ((InputLine = Reader.ReadLine()) != null)
+                    {
+                        await TOW_Chat.SendMessageAsync($"**[UBGE-TOW]** **|** ``{DateTime.Now}`` >> {InputLine.Replace(":UBGE-ToW!~UBGE-ToW@179.218.243.249 PRIVMSG #ubge.servidor :", "")}");
+                        string[] splitInput = InputLine.Split(new Char[] {
+                            ' '
+                        });
+                        if (splitInput[0] == "PING")
+                        {
+                            string Resposta = splitInput[1];
+                            Writer.WriteLine($"PONG {Resposta}");
+                            Writer.Flush();
+                        }
+
+                        switch (splitInput[1])
+                        {
+                            case "001":
+                                Writer.WriteLine($"JOIN {Canal}");
+                                Writer.Flush();
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                    Writer.Close();
+                    Reader.Close();
+                    IRC.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"[IRC] [UBGE-TOW] [Wall-E] [Discord] | A conexão com o servidor: \"TOW\" não foi estabelecida com sucesso.\n.\nErro: {ex.ToString()}");
+                Console.ResetColor();
+                await Secretaria_OpenSpades.SendMessageAsync($"**[IRC] [UBGE-TOW] [Wall-E] [Discord]** **|** A conexão com o servidor: **\"TOW\"** não foi estabelecida com sucesso. <@&{valores.OpenSpades}>, procurem o erro e tentem resolver!\n.\n**Erro:** {ex.ToString()}");
+                await Log.SendMessageAsync($"**[IRC] [UBGE-TOW] [Wall-E] [Discord]** **|** A conexão com o servidor: **\"TOW\"** não foi estabelecida com sucesso.\n.\n**Erro:** {ex.ToString()}");
+                Thread.Sleep(5000);
+                string[] argv = { };
+            }
+        }
+    }
+}
